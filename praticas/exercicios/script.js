90cc0af5ce5d875c047027ba71775b594c4adad1
Rômulo Queiroz <rfcontatosvia@gmail.com>
2022-09-19 20:42:10 -0300

    study
    Estudando

--- praticas/exercicios/script.js
+++ praticas/exercicios/script.js
@@ -1,14 +1,22 @@
 function meuEscopo (){
     const form = document.querySelector('.form');
-    // form.onsubmit = function(evento){
-    //     evento.preventDefault();
-    //     console.log('Foi enviado');
-    // };
-    let contador = 1;
+    const resultado = document.queryCommandIndeterm('.resultado');        
+    const pessoas = [];                         
     function recebeEventoForm(evento){
         evento.preventDefault();
-        console.log(`Form não enviado.${contador}`)
-        contador++;
+
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const Altura = form.querySelector('.altura');
+        
+        pessoas.push({
+            nome: nome.Value,
+            Sobrenome: sobrenome.Value,
+            peso: peso.Altura,
+            Altura: Altura.Value
+        });
+        console.log(pessoas)
     }
     form.addEventListener('submit', recebeEventoForm);
 }
