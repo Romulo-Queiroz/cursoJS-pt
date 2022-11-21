ff37e17e0efe595502fb22065636de6f57931873
Rômulo Queiroz <rfcontatosvia@gmail.com>
2022-11-21 13:56:05 -0300

    Calculadora com js
    Utilizando factory function
    Time da aula: 39:50 min

---
+++ JS-Functions-Avançado/calculadora/calculadora.js
@@ -0,0 +1,69 @@
+function criaCalculadora () {
+    return {
+        display : document.querySelector('.display'),
+        
+        inicia() {
+            this.cliqueBotoes();
+            this.pressionaEnter();
+        },
+        
+        pressionaEnter(){
+            this.display.addEventListener('keypress', e => {
+                if (e.keyCode === 13) {
+                    this.realizaConta();
+                }
+            });
+        },
+
+        realizaConta(){
+            let conta = this.display.value;
+
+            try {
+                conta = eval(conta);
+
+                if(!conta) {
+                    alert('Conta Inválida');
+                    return;
+                }
+
+              this.display.value = conta;
+            } catch(e) {
+                alert('Conta Inválida');
+            }
+        },
+        clearDisplay() {
+            this.display.value = '';
+        },
+        apagaUm() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        cliqueBotoes() {
+            document.addEventListener('click',  e => {
+                const el = e.target;
+
+                if(el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }    
+
+                if(el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                }
+
+                if(el.classList.contains('btn-del')) {
+                    this.apagaUm();
+                }
+                if(el.classList.contains('btn-eq')) {
+                    this.realizaConta();
+                }
+            });
+        },
+
+        btnParaDisplay(valor){
+            this.display.value += valor;
+        }
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
