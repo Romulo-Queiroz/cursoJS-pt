fad7d8f21f58ea45a5c1b31202082c7d9e142512
Rômulo Queiroz <rfcontatosvia@gmail.com>
2022-12-03 12:15:32 -0300

    Object
    defineProperty
     defineProperties

--- Javascript/seção-6(avançado)/definePropety.js
+++ Javascript/seção-6(avançado)/definePropety.js
@@ -1,3 +1,46 @@
+//defineProperty 
+
+function Produto (nome, preco, estoque) {
+    this.nome = nome;
+    this.preco = preco;
+    this.estoque = estoque;
+
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, //mostrar chave ?
+        value: estoque, //acessando valor
+        writable: false, //posso alterar ?
+        configurable: true // é configurável ?
+    });
+}
+
+const p1 = new Produto('Camiseta', 20.00, 3);
+p1.estoque = 50000; // whitable precisa estar habilitado para alterar value
+console.log(p1);
+
+// defineProperties 
+
+function Produtos (nome, preco, estoque) {
+    this.nome = nome;
+    this.preco = preco;
+    this.estoque = estoque;
+
+    Object.defineProperties(this, {
+        nome:{
+        enumerable: true, //mostrar chave ?
+        value: nome, //acessando valor
+        writable: false, //posso alterar ?
+        configurable: true, // é configurável ?},
+
+        preco:{
+        enumerable: true, //mostrar chave ?
+        value: preco, //acessando valor
+        writable: false, //posso alterar ?
+        configurable: true // é configurável ?
+        },  
+    }
+    });
+}
 
 
-function produto (no)
+const p2 = new Produtos('Camiseta', 20.00, 3);
+console.log(p2);
