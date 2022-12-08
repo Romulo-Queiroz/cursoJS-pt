b92139d5c2dba7a3486857b4087b7a0eba2105bd
Rômulo Queiroz <rfcontatosvia@gmail.com>
2022-12-08 11:13:05 -0300

    init: iniciando projeto

---
+++ Javascript/seçao-7 (avançado)/index.js
@@ -0,0 +1,7 @@
+const nome = document.querySelector('.nome');
+const sobrenome = document.querySelector('sobenome');
+const cpf = document.querySelector('.cpf');
+const usuario = document.querySelector('.usuario');
+const senha = document.querySelector('.senha');
+const repetirSenha = document.querySelector('.repetir-senha');
+
