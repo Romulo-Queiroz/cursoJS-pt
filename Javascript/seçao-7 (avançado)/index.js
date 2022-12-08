c2a63b94e02526ea251b6e6a5ea30fdec67f7808
Rômulo Queiroz <rfcontatosvia@gmail.com>
2022-12-08 11:56:41 -0300

    feat: adicionando mensagens de erros

--- Javascript/seçao-7 (avançado)/index.js
+++ Javascript/seçao-7 (avançado)/index.js
@@ -1,7 +1,33 @@
-const nome = document.querySelector('.nome');
-const sobrenome = document.querySelector('sobenome');
-const cpf = document.querySelector('.cpf');
-const usuario = document.querySelector('.usuario');
-const senha = document.querySelector('.senha');
-const repetirSenha = document.querySelector('.repetir-senha');
+class ValidaFormulario {
+  constructor() {
+    this.formulario = document.querySelector(".formulario");
+    this.eventos();
+  }
+  eventos() {
+    this.formulario.addEventListener("submit", (e) => {
+      this.handleSubmit(e);
+    });
+  }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    const camposValidos = this.camposSaoValidos();
+  }
+
+  camposSaoValidos() {
+    let valid = true;
+
+    for (let campo of this.formulario.querySelectorAll(".validar"))
+      if (!campo.value) {
+        this.criaErro(campo, "Campo tal não pode estar em branco.");
+      }
+  }
+  criaErro(campo, msg) {
+    const div = document.createElement('div');
+    div.innerHTML = msg;
+    div.classList.add('Error-text');
+    campo.insertAdjacentElement('afterend',div);
+  }
+}
+
+const valida = new ValidaFormulario();
