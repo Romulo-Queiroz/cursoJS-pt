977497f3c87139af45d8e553d81cf4659bc01db1
Rômulo Queiroz <rfcontatosvia@gmail.com>
2022-12-30 17:37:15 -0300

    build: o que houve aq?

--- node/app.js
+++ node/app.js
@@ -1,8 +1,36 @@
 const express = require("express");
+const { randomUUID } = require("crypto");
 const app = express();
 
-app.get("/primeira-rota", (request, response) => {
-  return response.json({ message: "Rota de produto" });
+app.use(express.json());
+
+const products = [];
+
+/*
+ *POST => Inserir produtos
+ *GET => Buscar produtos
+ *PUT => Alterar produtos
+ *DELETE => Deletar produtos
+ */
+
+/*
+ *body sempre que quero enviar dados para minha aplicação
+ *params /products/:id sempre que quero buscar um produto especifico (parametros de rota)
+ *query => /product?id=123
+ */
+
+app.post("/products", (request, response) => {
+  const { name, price } = request.body;
+
+  const product = {
+    name,
+    price,
+    id: randomUUID(),
+  };
+
+  products.push(product);
+
+  return response.json(product);
 });
 
 app.listen(4002, () => console.log("Servidor esta rodando na porta 4002"));
