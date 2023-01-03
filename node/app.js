cfd489aab169b0ef1969a5c1a423f04bb5d617b4
Rômulo Queiroz <rfcontatosvia@gmail.com>
2023-01-03 20:00:34 -0300

    build: PUT, DELET, GET, POST

--- node/app.js
+++ node/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const crypto = require("crypto");
 const app = express();
 
 app.use(express.json());
@@ -25,7 +25,7 @@ app.post("/products", (request, response) => {
   const product = {
     name,
     price,
-    id: Math.random().toFixed(1),
+    id: crypto.randomBytes(16).toString("hex"),
   };
 
   products.push(product);
@@ -37,4 +37,34 @@ app.get("/products", (request, response) => {
   return response.json(products);
 });
 
+app.get("/products/:id", (request, response) => {
+  const { id } = request.params;
+
+  const product = products.find((product) => product.id === id);
+
+  return response.json(product);
+});
+
+app.put("/products/:id", (request, response) => {
+  const { id } = request.params;
+  const { name, price } = request.body;
+
+  const productIndex = products.findIndex((product) => product.id === id);
+  products[productIndex] = {
+    ...products[productIndex],
+    name,
+    price,
+  };
+  return response.json({ message: "Produto alterado com sucesso" });
+});
+
+app.delete("/products/:id", (request, response) => {
+  const { id } = request.params;
+
+  const productIndex = products.findIndex((product) => product.id === id);
+
+  products.splice(productIndex, 1);
+
+  return response.json({ message: "Produto deletado com sucesso" });
+});
 app.listen(4002, () => console.log("Servidor esta rodando na porta 4002"));
