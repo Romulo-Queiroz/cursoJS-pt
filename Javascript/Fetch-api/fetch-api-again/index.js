d06ee80bfb288b06e8daf865675d38e273127a70
Rômulo Queiroz <rfcontatosvia@gmail.com>
2023-01-17 20:50:14 -0300

    build: loader antes de carregar a pagina e após disparar evento

--- Javascript/Fetch-api/fetch-api-again/index.js
+++ Javascript/Fetch-api/fetch-api-again/index.js
@@ -6,6 +6,24 @@ function renderPage() {
   let loaderDiv = document.getElementsByClassName("loader");
   loaderDiv[0].style.display = "none";
   render.innerHTML = texto;
+  criaButton();
+}
+
+function criaButton() {
+  let button = document.createElement("button");
+  button.innerHTML = "Clique aqui";
+  button.className = "btn";
+  render.appendChild(button);
+
+  button.addEventListener("click", () => {
+    let loaderDiv = document.getElementsByClassName("loader");
+    loaderDiv[0].style.display = "block";
+    setTimeout(() => {
+      loaderDiv[0].style.display = "none";
+      texto = "Meu texto aqui depois do clique";
+      render.innerHTML = texto;
+    }, 4000);
+  });
 }
 
 function loader() {
